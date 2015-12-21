a01e1ba01fb2e028182978373eaa2c4650728c4e
Sakari Kapanen <sakari.m.kapanen@gmail.com>
2015-12-21 12:36:08 +0200

    Better overall control

--- balancer.ino
+++ balancer.ino
@@ -1,5 +1,8 @@
 // Based on Jeff Rowberg's MPU6050_DMP6.ino at
 // https://github.com/jrowberg/i2cdevlib
+//
+// The control scheme heavily inspired by B-robot:
+// https://github.com/jjrobots/B-ROBOT/blob/master/BROBOT/BROBOT.ino
 #include <I2Cdev.h>
 #include <MPU6050_6Axis_MotionApps20.h>
 #include <Wire.h>
@@ -7,18 +10,13 @@
 #include <NewPing.h>
 #include <IRremote.h>
 
+// IR button codes
 const unsigned long IR_OK    = 0xFF02FD,
                     IR_UP    = 0xFF629D,
                     IR_DOWN  = 0xFFA857,
                     IR_LEFT  = 0xFF22DD,
                     IR_RIGHT = 0xFFC23D,
                     IR_REP   = 0xFFFFFFFF;
-
-const double Kp = 1200, Ki = 5000, Kd = 60;
-const double SP_EQ = 0.19, SP_FWD = 0.23, SP_RIGHT = 0.19, SP_LEFT = 0.2;
-const double TURN_LEFT = -10, TURN_RIGHT = 8;
-double turnParam = 0;
-double setPoint = SP_EQ, input, output;
 
 // Pin mapping
 const int IR_PIN = 7,
@@ -30,19 +28,29 @@ const int IR_PIN = 7,
           MOTOR_BA = 9,
           MOTOR_BB = 5;
 
+/* const double SP_EQ = 0.19, SP_FWD = 0.23, SP_RIGHT = 0.19, SP_LEFT = 0.2; */
+const double Kpa = 0.001, Kia = 0.0012, Kda = 0,
+             Kpm = 1200, Kim = 5000, Kdm = 60,
+             gyroWeight = 0.2, lowPass = 0.05;
+double targetSpeed = 0, filteredSpeed = 0, targetPitch = 0, pitch = 0,
+       motorSpeed = 0;
+PID pitchPID(&filteredSpeed, &targetPitch, &targetSpeed, Kpa, Kia, Kda, DIRECT);
+PID motorPID(&pitch, &motorSpeed, &targetPitch, Kpm, Kim, Kdm, REVERSE);
+const int PID_SAMPLE_TIME = 10;
+
+// Some parameters for the motor drive
+int motorAspeed = 0, motorBspeed = 0;
+const double THROTTLE_FORWARD = 110, THROTTLE_BACKWARD = -110,
+             THROTTLE_TURN = 90;
+const double TURN_LEFT = -13, TURN_RIGHT = 17, TRIM = 4;
+double turnParam = 0;
+
 MPU6050 mpu;
-PID myPID(&input, &output, &setPoint, Kp, Ki, Kd, REVERSE);
 NewPing sonar(PING_TRIG, PING_ECHO, 50);
 IRrecv ir(IR_PIN);
 decode_results irRes;
 
-int motorAspeed = 0,
-    motorBspeed = 0;
-const float motorAgain = 1.05,
-            motorBgain = 1;
-
-bool ledState = LOW;
-
+// DMP helper variables
 bool dmpReady = false;
 uint8_t mpuIntStatus;
 uint8_t devStatus;
@@ -52,7 +60,7 @@ uint8_t fifoBuffer[64]; // FIFO storage buffer
 
 Quaternion q;
 VectorFloat gravity;
-float ypr[3] = { 0, 0, 0 };  // [yaw, pitch, roll]
+int16_t gyro[3] = { 0, 0, 0 };
 
 volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
 void dmpDataReady() {
@@ -60,8 +68,8 @@ void dmpDataReady() {
 }
 
 void updateMotors() {
-    updateMotorPWM(motorAgain * (motorAspeed + turnParam), MOTOR_AA, MOTOR_AB);
-    updateMotorPWM(motorBgain * (motorBspeed - turnParam), MOTOR_BA, MOTOR_BB);
+    updateMotorPWM(motorAspeed + turnParam, MOTOR_AA, MOTOR_AB);
+    updateMotorPWM(motorBspeed - turnParam, MOTOR_BA, MOTOR_BB);
 }
 
 void updateMotorPWM(int speed, const int pinA, const int pinB) {
@@ -75,105 +83,116 @@ void updateMotorPWM(int speed, const int pinA, const int pinB) {
 }
 
 void setup() {
+    // A little initial delay to get the robot's lid closed
     delay(4000);
+    // Pin setup
     pinMode(MOTOR_AA, OUTPUT);
     pinMode(MOTOR_AB, OUTPUT);
     pinMode(MOTOR_BA, OUTPUT);
     pinMode(MOTOR_BB, OUTPUT);
     pinMode(BLUE_LED_PIN, OUTPUT);
-    updateMotors();
-    myPID.SetMode(AUTOMATIC);
-    myPID.SetOutputLimits(-255, 255);
-    myPID.SetSampleTime(10);
 
+    updateMotors();
     ir.enableIRIn();
 
-    /* Serial.begin(9600); */
+    pitchPID.SetMode(AUTOMATIC);
+    pitchPID.SetOutputLimits(-0.25, 0.25);
+    pitchPID.SetSampleTime(PID_SAMPLE_TIME);
+    motorPID.SetMode(AUTOMATIC);
+    motorPID.SetOutputLimits(-255, 255);
+    motorPID.SetSampleTime(PID_SAMPLE_TIME);
+
+    #ifdef DEBUG_ON
+        Serial.begin(9600);
+    #endif
     Wire.begin();
     mpu.initialize();
     devStatus = mpu.dmpInitialize();
 
-    mpu.setXGyroOffset(220);
-    mpu.setYGyroOffset(76);
-    mpu.setZGyroOffset(-85);
-    mpu.setZAccelOffset(1788);
+    mpu.setXAccelOffset(-2946);
+    mpu.setYAccelOffset(1570);
+    mpu.setZAccelOffset(1482);
+    mpu.setXGyroOffset(74);
+    mpu.setYGyroOffset(8);
+    mpu.setZGyroOffset(25);
 
-    // make sure it worked (returns 0 if so)
-    if (devStatus == 0) {
-        mpu.setDMPEnabled(true);
-        attachInterrupt(0, dmpDataReady, RISING);
-        mpuIntStatus = mpu.getIntStatus();
-        packetSize = mpu.dmpGetFIFOPacketSize();
-
-        delay(10000);
-        dmpReady = true;
+    if (devStatus != 0) {
+        #ifdef DEBUG_ON
+            Serial.println("DMP initialization failed");
+        #endif
+        return;
     }
-}
 
-void toggleLed() {
-    ledState = ledState == HIGH ? LOW : HIGH;
-    digitalWrite(BLUE_LED_PIN, ledState);
+    mpu.setDMPEnabled(true);
+    attachInterrupt(0, dmpDataReady, RISING);
+    mpuIntStatus = mpu.getIntStatus();
+    packetSize = mpu.dmpGetFIFOPacketSize();
+
+    delay(5000);  // A delay for MPU calibration
+    dmpReady = true;
 }
 
 void loop() {
-    // if programming failed, don't try to do anything
     if (!dmpReady) return;
+    while (!mpuInterrupt && fifoCount < packetSize) mainLoop();
+    readMPU();
+}
 
-    // wait for MPU interrupt or extra packet(s) available
-    while (!mpuInterrupt && fifoCount < packetSize) {
-        input = ypr[1];
-        myPID.Compute();
-        motorAspeed = motorBspeed = output;
-        updateMotors();
+void mainLoop() {
+    pitchPID.Compute();
+    motorPID.Compute();
+    motorAspeed = motorBspeed = motorSpeed;
+    updateMotors();
 
-        if (ir.decode(&irRes)) {
-            switch (irRes.value) {
-                case IR_OK:
-                    setPoint = SP_EQ;
-                    turnParam = 0;
-                    break;
-                case IR_UP:
-                    setPoint = SP_FWD;
-                    turnParam = 0;
-                    break;
-                case IR_LEFT:
-                    turnParam = TURN_LEFT;
-                    setPoint = SP_LEFT;
-                    break;
-                case IR_RIGHT:
-                    turnParam = TURN_RIGHT;
-                    setPoint = SP_RIGHT;
-                    break;
-            }
-            ir.resume();
+    if (ir.decode(&irRes)) {
+        switch (irRes.value) {
+            case IR_OK:
+                targetSpeed = 0;
+                turnParam = TRIM;
+                break;
+            case IR_UP:
+                targetSpeed = THROTTLE_FORWARD;
+                turnParam = 0;
+                break;
+            case IR_DOWN:
+                targetSpeed = THROTTLE_BACKWARD;
+                turnParam = TRIM;
+                break;
+            case IR_LEFT:
+                turnParam = TURN_LEFT;
+                targetSpeed = THROTTLE_TURN;
+                break;
+            case IR_RIGHT:
+                turnParam = TURN_RIGHT;
+                targetSpeed = THROTTLE_TURN;
+                break;
         }
+        ir.resume();
     }
+}
 
-    // reset interrupt flag and get INT_STATUS byte
+void readMPU() {
     mpuInterrupt = false;
     mpuIntStatus = mpu.getIntStatus();
-
-    // get current FIFO count
     fifoCount = mpu.getFIFOCount();
 
-    // check for overflow (this should never happen unless our code is too inefficient)
     if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
-        // reset so we can continue cleanly
         mpu.resetFIFO();
-    // otherwise, check for DMP data ready interrupt (this should happen frequently)
     } else if (mpuIntStatus & 0x02) {
-        // wait for correct available data length, should be a VERY short wait
         while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
-
-        // read a packet from FIFO
         mpu.getFIFOBytes(fifoBuffer, packetSize);
-
-        // track FIFO count here in case there is > 1 packet available
-        // (this lets us immediately read more without waiting for an interrupt)
         fifoCount -= packetSize;
 
         mpu.dmpGetQuaternion(&q, fifoBuffer);
         mpu.dmpGetGravity(&gravity, &q);
-        mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+        mpu.dmpGetGyro(gyro, fifoBuffer);
+        // We only need the pitch - hence no call to getYawPitchRoll
+        pitch = atan(gravity.x / sqrt(gravity.y*gravity.y + gravity.z*gravity.z));
+        double speedEstimate = motorSpeed + gyroWeight * gyro[1];
+        filteredSpeed = filteredSpeed -
+            lowPass * (filteredSpeed - speedEstimate);
+        #ifdef DEBUG_ON
+            Serial.println(gyro[1]);
+        #endif
     }
 }
